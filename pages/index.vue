830de41a5afc4d2cd01a8a78c06fa710cc8f73ed
Alberto  Rodriguez <alberto.rodriguez.davila@gmail.com>
2021-09-17 15:33:24 -0500

    landing base

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home home-1" id="list-of-sections">
+    <Header1 />
+    <Banner1 />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header1 from '@/components/Header1.vue'
+import Banner1 from '@/components/home1/Banner1.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    Header1,
+    Banner1,
+    Footer,
+  }
+}
+</script>
