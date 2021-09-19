eda5eebb7acfaf12402635aed62d1c700ae73775
Alberto  Rodriguez <alberto.rodriguez.davila@gmail.com>
2021-09-19 17:11:53 -0500

    header

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="home home-1" id="list-of-sections">
     <Header1 />
     <Banner1 />
+    <About/>
     <Footer />
   </div>
 </template>
@@ -10,12 +11,14 @@
 // @ is an alias to /src
 import Header1 from '@/components/Header1.vue'
 import Banner1 from '@/components/home1/Banner1.vue'
+import About from '@/components/home1/About.vue'
 import Footer from '@/components/Footer.vue'
 
 export default {
   components: {
     Header1,
     Banner1,
+    About,
     Footer,
   }
 }
