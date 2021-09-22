30c0315307012a9521adcd4077f272e8d177b37c
Alberto  Rodriguez <alberto.rodriguez.davila@gmail.com>
2021-09-22 14:17:05 -0500

    our benefits

---
+++ pages/service/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home home-1" id="list-of-sections">
+    <Header1/>
+    <Footer/>
+  </div>
+</template>
+<script>
+import Header1 from '@/components/Header1.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+  components: {
+    Header1,
+    Footer
+  }
+}
+</script>
