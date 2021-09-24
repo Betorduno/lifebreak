25f3cefe12ff81e77109bc4f6d9ecc20c71ec70f
nandorodriguez <fernan2rodriguezdav@gmail.com>
2021-09-24 13:45:18 -0500

    Blog

---
+++ pages/blog.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home home-1" id="list-of-sections">
+    <Header1 />
+    <Banner title="Blog"/>
+    <BlogClassicSection />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header1 from '@/components/Header1.vue'
+import Banner from '@/components/contacts/Banner.vue'
+import BlogClassicSection from '@/components/blogs/BlogClassicSection.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'BlogClassic',
+  components: {
+    Header1,
+    Banner,
+    BlogClassicSection,
+    Footer,
+  }
+}
+</script>
---
+++ pages/single-blog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home home-1" id="list-of-sections">
+    <Header1 />
+    <Banner title="Blogs"/>
+    <BlogSinglePageLayout />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header1 from '@/components/Header1.vue'
+import Banner from '@/components/contacts/Banner.vue'
+import BlogSinglePageLayout from '@/components/blogs/BlogSinglePageLayout.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'BlogSingle',
+  components: {
+    Header1,
+    Banner,
+    BlogSinglePageLayout,
+    Footer,
+  },
+
+}
+</script>
